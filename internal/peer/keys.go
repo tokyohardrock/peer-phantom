4126f5e4f8921abe716656da51e2554c1a5aeae5
tokyohardrock <tokyohardrock@gmail.com>
2026-06-03 14:45:35 +0300

    returned identity saving

--- internal/peer/keys.go
+++ internal/peer/keys.go
@@ -3,6 +3,7 @@ package peer
 import (
 	"crypto/rand"
 	"fmt"
+	"os"
 
 	"github.com/libp2p/go-libp2p/core/crypto"
 )
@@ -10,29 +11,29 @@ import (
 func loadPrivateKey(keyFile string) (crypto.PrivKey, error) {
 	const fn = "peer.loadPrivateKey"
 
-	//if _, err := os.Stat(keyFile); os.IsNotExist(err) {
-	privKey, _, err := crypto.GenerateEd25519Key(rand.Reader)
-	if err != nil {
-		return nil, fmt.Errorf("%s: failed to generate private key: %w", fn, err)
+	if _, err := os.Stat(keyFile); os.IsNotExist(err) {
+		privKey, _, err := crypto.GenerateEd25519Key(rand.Reader)
+		if err != nil {
+			return nil, fmt.Errorf("%s: failed to generate private key: %w", fn, err)
+		}
+
+		keyAsBytes, err := crypto.MarshalPrivateKey(privKey)
+		if err != nil {
+			return nil, fmt.Errorf("%s: failed to marshal private key: %w", fn, err)
+		}
+
+		err = os.WriteFile(keyFile, keyAsBytes, 0600)
+		if err != nil {
+			return nil, fmt.Errorf("%s: failed to write private key to file: %w", fn, err)
+		}
+
+		return privKey, nil
 	}
 
-	// keyAsBytes, err := crypto.MarshalPrivateKey(privKey)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("%s: failed to marshal private key: %w", fn, err)
-	// }
+	privKey, err := os.ReadFile(keyFile) // load existing key
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed to read private key from file: %w", fn, err)
+	}
 
-	// err = os.WriteFile(keyFile, keyAsBytes, 0600)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("%s: failed to write private key to file: %w", fn, err)
-	// }
-
-	return privKey, nil
-	//}
-
-	// privKey, err := os.ReadFile(keyFile) // load existing key
-	// if err != nil {
-	// 	return nil, fmt.Errorf("%s: failed to read private key from file: %w", fn, err)
-	// }
-
-	// return crypto.UnmarshalPrivateKey(privKey)
+	return crypto.UnmarshalPrivateKey(privKey)
 }

0c9283f88777c5d106d4aee41cc61132122e020c
tokyohardrock <tokyohardrock@gmail.com>
2026-06-06 21:14:09 +0300

    add message length limit and decreased prefix length

--- internal/utils/prefix.go
+++ internal/utils/prefix.go
@@ -6,17 +6,22 @@ import (
 	"io"
 )
 
+const (
+	messageLengthLimit = 10 * 1024 // 10 Kb
+	prefixLen          = 2
+)
+
 func ReadMessageWithLengthPrefix(reader io.Reader) ([]byte, error) {
 	const fn = "peer.readMessageWithPrefix"
 
-	prefix := make([]byte, 4)
+	prefix := make([]byte, prefixLen)
 
 	_, err := reader.Read(prefix)
 	if err != nil {
 		return nil, fmt.Errorf("%s during reading message length from stream: %w", fn, err)
 	}
 
-	msgLen := binary.BigEndian.Uint32(prefix)
+	msgLen := binary.BigEndian.Uint16(prefix)
 	rawMessage := make([]byte, msgLen)
 
 	_, err = reader.Read(rawMessage)
@@ -27,12 +32,18 @@ func ReadMessageWithLengthPrefix(reader io.Reader) ([]byte, error) {
 	return rawMessage, nil
 }
 
-func AddLengthPrefixToMessage(message []byte) []byte {
-	prefix := make([]byte, 4)
-	binary.BigEndian.PutUint32(prefix, uint32(len(message)))
+func AddLengthPrefixToMessage(message []byte) ([]byte, error) {
+	const fn = "utils.AddLengthPrefixToMessage"
 
-	messageWithPrefix := make([]byte, 4+len(message))
+	if len(message) > messageLengthLimit {
+		return nil, fmt.Errorf("%s: message is to big (max %d bytes)", fn, messageLengthLimit)
+	}
+
+	prefix := make([]byte, prefixLen)
+	binary.BigEndian.PutUint16(prefix, uint16(len(message)))
+
+	messageWithPrefix := make([]byte, prefixLen+len(message))
 	messageWithPrefix = append(prefix, message...)
 
-	return messageWithPrefix
+	return messageWithPrefix, nil
 }
